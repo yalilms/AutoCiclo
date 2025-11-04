fac3fbf7a446da39fbd292918969cb8801439506
yalilms <yalil.ms72@gmail.com>
2025-11-04 13:25:30 +0100

    Initial commit

---
+++ app/src/main/java/com/javafx/A_holamundofxml/Main.java
@@ -0,0 +1,30 @@
+package com.javafx.A_holamundofxml;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Molina
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primeraEscena) throws Exception { //puede lanzar excep'
+        Parent root = FXMLLoader.load(getClass().getResource("/ejemplo_holamundo.fxml"));
+
+        Scene scene = new Scene(root);
+        primeraEscena.setScene(scene);
+        primeraEscena.setTitle("Primer Ejemplo JavaFX - Hola Mundo");
+        primeraEscena.show();
+        
+        
+    }
+}
---
+++ app/src/main/java/com/javafx/A_holamundofxml/miControlador.java
@@ -0,0 +1,29 @@
+package com.javafx.A_holamundofxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Molina
+ */
+public class miControlador implements Initializable {
+
+    @FXML
+    private Label miEtiqueta;
+
+    @FXML
+    private void botonPulsado(ActionEvent event) {
+        miEtiqueta.setText("Hola Mundo");
+        System.out.println("Hola Mundo!!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        miEtiqueta.setText("");
+    }
+}
