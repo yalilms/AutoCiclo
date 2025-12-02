98a61f204c5cd6da964f84dbfbb54d4d31e11c64
yalilms <yalil.ms72@gmail.com>
2025-12-02 13:09:21 +0100

    añadida logo pantalla de carga en base 64

--- app/src/main/java/com/autociclo/Main.java
+++ app/src/main/java/com/autociclo/Main.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
 import javafx.animation.KeyFrame;
 import javafx.util.Duration;
